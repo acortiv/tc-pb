8865419e4bbe40bfd8ff30c22a314e1f2a88babf
acortiv <tacortiv@proton.me>
2024-04-07 14:01:42 -0400

    idk

--- client/app/projects/page.tsx
+++ client/app/projects/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "../components/footer";
 import Nav from "../components/nav";
-import ProjectList from "../components/project-list";
+import ProjectList from "@/app/components/project-list";
 
 export default function Projects() {
   return (

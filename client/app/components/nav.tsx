0384f8fcb552c1c95c65e1383bd7ad08bd76f5e8
acortiv <tacortiv@proton.me>
2024-04-07 20:58:13 -0400

    more more small changes

--- client/app/components/nav.tsx
+++ client/app/components/nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-      <header className="nav-container mt-3 mb-3">
+      <header className="nav-container mt-3 mb-3 ml-1">
         <h1><Link href="/">[Home]</Link></h1>
         <h1><Link href="/projects">[Projects]</Link></h1>
         <h1><Link href="/resume">[Resume]</Link></h1>

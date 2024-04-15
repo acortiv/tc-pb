7647054424e6d7ed332d1674d05937119db3e00f
acortiv <tacortiv@proton.me>
2024-04-15 14:26:42 -0400

    spelling errors

--- client/app/components/recent-project.tsx
+++ client/app/components/recent-project.tsx
@@ -2,7 +2,7 @@ export default function RecentProject() {
     return (
       <section className="p-4">
         <h2 className="text-[#00cc00] text-3xl mb-4">Latest Projects</h2>
-        <p>I am currently working on an eCommerce platform hosted by Amazon Product Advertising Affiliates.  The site makes use of a wide range of technologies to automate data collection related and simplify the administrative operations for my clients.</p>
+        <p>I am currently working on an eCommerce platform hosted by Amazon Product Advertising Affiliates.  The site makes use of a wide range of technologies to automate data collection and simplify the administrative operations for my clients.</p>
       </section>
     );
 }

566ce165c0408bdb6e39e52bffec632fef2d4304
acortiv <tacortiv@proton.me>
2024-04-15 14:38:58 -0400

    spelling errors

--- client/app/components/skills.tsx
+++ client/app/components/skills.tsx
@@ -2,7 +2,7 @@ export default function SkillsAndExperience() {
     return (
       <section className="p-4 bg-[#1a1a2e] mt-6">
         <h2 className="text-[#00cc00] text-3xl mb-4">Skills & Experience</h2>
-        <p>Throughout my academic and professional career I have been exposed to a broad range of subjects within Finance & technologies seen in modern Software Development.</p>
+        <p>Throughout my academic and professional career, I have been exposed to a broad range of subjects within Finance & technologies seen in modern Software Development.</p>
       </section>
     );
 }
